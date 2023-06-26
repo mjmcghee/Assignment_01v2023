931bb8ee188a1faf6d3e0d6ce67a041f111b1039
Marcus McGhee <marcusjm@gmail.com>
2023-06-25 20:06:25 -0700

    Assignment 01 Marcus McGhee

---
+++ js/script.js
@@ -0,0 +1,112 @@
+// STEP 1
+    // let someMonth;
+    // function theMonth();
+    // let currentMonth;
+    // let summerMonth;
+    // let myLibraryFunction;
+// STEP 2
+    //  let aNumber = 4;
+    //  let aString = `This is a sentence.`;
+    //  let aFact = True;
+    //  let maybeEmpty = null;
+// STEP 3
+    // let yourNumber = (x,y) => {return x + y};
+    // let yourName = (nameInput) => {return `Your name is ${nameInput}`};
+// STEP 4
+    // let firstName;
+    // let lastName;
+    // let homeAddress;
+    // let homeCity;
+    // let homeState;
+    // let homeZipCode;
+    // let yourAge;
+    // let referralInfo;
+    // let contactInfo;
+// STEP 5
+    // firstName = 'Marcus';
+    // yourAge = 35;
+    // homeState = 'California';
+    // let accInfo = `Your name is ${firstName} and you are ${yourAge} years old. You live in ${homeState}`;
+    // console.log(accInfo);
+// STEP 6
+    // let inputName = prompt("What is your name?");
+    // let inputAge = prompt("What is your age?");
+    // let yourInfo = `Your name is ${inputName} and you are ${inputAge} years old.`;
+    // console.log(yourInfo);
+// STEP 7
+    // example 1
+    // let todayDate = Date.now;
+    // console.log(todayDate);
+    // example 2
+    // let aNumString = 2;
+    // let inputString = `What does ${aNumString} + ${aNumString} equal?`;
+    // console.log(inputString);
+    // console.log(`The answer is ${aNumString + aNumString}`);
+// STEP 8
+    // Corrected someString --> Added a backslash before the prarenthesis in (I'm)
+    // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+    // window.console.log(someString);
+//STEP 9
+    // let newVariable = null;
+    // console.log(newVariable);
+    // let anotherVariable;
+    // console.log(anotherVariable);
+// STEP 10
+    // let thisYear = new Date;
+    // thisYear.setFullYear(2023);
+    // console.log(`This year is ${thisYear}`);
+    // let aNumber = 10;
+    // console.log(aNumber);
+    // console.log(+false);
+    // console.log(+null);
+    // let anObject = {};
+    // console.log(anObject);
+    // let notAVar;
+    // console.log(notAVar);
+// STEP 11
+    // console.log('Hello' + ' Marcus McGhee' + ', welcome to the Javascript class!');
+// STEP 12
+    // let newName = 'Marcus McGhee';
+    // alert(`Your name is ${newName}.`)
+// STEP 13
+    // let courseName = 'Javascript';
+    // console.log('Hello' + ` ${newName}` + `, welcome to the ${courseName} class!`);
+// STEP 14
+    // console.log('Hello' + ` ${newName}` + `, \nWelcome to the ${courseName} class!`);
+// STEP 15
+    // let newName = prompt('Enter your name: ');
+// STEP 16
+    // let courseName = prompt('Enter the name of th course: ');
+// STEP 17
+    // let newVarX = 10;
+    // let newVarY = 20;
+    // let newSum = newVarX + newVarY;
+    // console.log(`The sum of the two variables is ${newSum}.`)
+// STEP 18
+    // let xVar = 20;
+    // console.log(xVar);
+    // xVar = xVar + 20;
+    // console.log(xVar);
+// STEP 19
+    // let xVar = 20;
+    // console.log(xVar);
+    // xVar = xVar * 5;
+    // console.log(xVar);
+// STEP 20
+    // let xVar = 20%3;
+    // console.log(xVar);
+// STEP 21
+    // let nameVar = 'Marcus';
+    // if (nameVar.length%2 === 0) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+// STEP 22
+    // let secretPwd = 'Marcus';
+    // let inputPwd = prompt('What is the secret password?')
+    // if (inputPwd !== secretPwd) {
+    //     console.log('You shall not pass. ' + false);
+    // } else {
+    //     console.log('You may enter. ' + true);
+    // }
